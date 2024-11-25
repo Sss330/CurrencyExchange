afb5de34504ebf9345be2dda2bff027f1a02d480
Sss330 <lordrom92@gmail.com>
2024-11-25 13:50:39 +0300

    initial commit

---
+++ src/main/java/org/example/currencyexchange/HelloServlet.java
@@ -0,0 +1,12 @@
+package org.example.currencyexchange;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+
+
+    }
